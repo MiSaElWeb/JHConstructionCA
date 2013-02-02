d07bc68ada45887b486d4648c218b06bd39e622d
Misael Gaytan <misael@misaelwebsites.com>
2013-02-01 23:53:42 -0800

    Created a Contact Us Page
    Create a contact us page in php and added a sidebar image

---
+++ contact_us.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta charset="UTF-8" />
+	<title>Fire Damage Restoration | JH Construction | Contact Us</title>
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link href="css/print.css" media="print" rel="stylesheet" />
+	<link href="css/style.css" media="screen" rel="stylesheet" />
+<meta name="description" content="JH Construction offers full fire damage restoration and  assistance with fire insurance claims." />
+<meta name="keywords" content="fire damage restoration, fire insurance claims, freeman and son, freeman son, freeman & son" />
+		</head>
+<body class="home ">
+	<div id="wrapper">
+		<div class="container" id="header">
+			<div id="site-logo">
+				<a href="/" title="JH COnstruction" rel="home">JH Construction</a>
+				<span><img src="img/print-logo.jpg" alt="" /></span>
+			</div>
+			<div class="header-right">
+				<p class="phone"><a href="tel:+1-800-278-4234">(951) 111-1111</a></p>
+				<div class="menu-top-container">
+					<ul id="menu-top" class="menu">
+						<li>
+							<a href="/">Home</a>
+						</li>
+						<li>
+							<a href="team.html">The Expert Team</a>
+						</li>
+						<li>
+							<a href="services.html">Services</a>
+						</li>
+						<li>
+							<a href="about_us.html">About Us</a>
+						</li>
+						<li>
+							<a href="contact_us.html">Contact Us</a>
+						</li>
+					</ul>
+				</div>			
+			</div>
+		<div class="clear-both"></div>
+		</div><hr />
+		<div id="navigation">
+			<div class="container">
+				<ul id="menu-header" class="menu">
+					<li>
+						<a href="what-to-expect.html">What To Expect</a>
+						<ul class="sub-menu">
+							<li>
+								<a href="getting-started.html">Getting Started</a>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a href="what-we-do.html">What We Do</a>
+						<ul class="sub-menu">
+							<li>
+								<a href="services.html">Our Services</a>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a href="what-we-done.html">What We&#8217;ve Done</a>
+						<ul class="sub-menu">
+							<li>
+								<a href="contact_us.html">More Information</a>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</div>
+			<div class="container" id="main">
+
+			<h1 class="page-title">Contact Us</h1>
+			<div id="content" role="main">
+									<div id="post-39" class="post-39 page type-page status-publish hentry page-content">
+						<div id="" class="vcard">
+<div class="org">JH Construction</div>
+<div class="adr">
+<div class="street-address">123 Address Ave</div>
+<p><span class="locality">City,</span>, <span class="region">CA</span> <span class="postal-code">92587</span></p>
+</div>
+</div>
+<p><strong>Phone (951) 111-1111<br /></strong></p>
+<p><strong>CSLB#  </strong></p>
+<p>&nbsp;</p>
+<p>Call Today:</p>
+<ul>
+<li>For <strong> Free No-Obligation Consultation</strong> about your unique situation</li>
+<li>For an ACCURATE assessment of your damage</li>
+<li>For an honest evaluation of your insurance company&#8217;s offer</li>
+<li>To regain some control over your life</li>
+<li>To get the money you are entitled to receive from your payments to the insurance company</li>
+<li>To let our expert team of professionals restore your home including personal property, while you get back to doing your business and living your life. </li>
+<li>For referrals to the best Public Adjusters in Southern California who can represent you in this unfamiliar territory .</li>
+</ul>
+					</div>
+					<div class="next-button">
+											</div>
+							</div>
+
+
+			<div id="sidebar" role="complementary">
+				<div id="text-2" class="widget widget_text">			<div class="textwidget"><a rel="shadowbox;width=400;height=600" class="bottom-link SBnoResize" href="contact_us.php"><img src="img/sidebar-button.jpg" border="0" alt="" /></a></div>
+		</div>							</div>			<div class="clear-both"></div>
+			<div class="bottom">
+				<div class="bottom-section first">
+					<div class="content">
+						<h3>Settlement Increase</h3>
+						<img src="http://www.freemanson.com/wp-content/themes/freemanson/img/settlement-increase.jpg" border="0" alt="" />
+						<p>Want to see how much we can potentially help you get? Click below to calculate your potential settlement increase.</p>
+					</div>
+					<p align="center"><a class="bottom-link" href="/calculator-work/">Click Here To Calculate &raquo;</a></p>
+				</div>
+				<div class="bottom-section">
+											<div class="content">
+							<h3>Before &amp; After</h3>
+							<img src="http://www.freemanson.com/wp-content/themes/freemanson/img/before-after-01.png" border="0" alt="" />
+						</div>
+						<p align="center"><a class="bottom-link" href="/what-weve-done/before-after/">View All Before &amp; After Photos &raquo;</a></p>
+									</div>
+				<div class="bottom-section last">
+											<div class="content">
+							<h3>Testimonial</h3>
+							<blockquote class="testimonial">
+																									<p>&#8220;After talking to everyone on my very, very long list, I hired Freeman &#038; Son&#8230;and am I ever glad I did! Every member of the team really cares.&#8221;</p>
+									- Sharon Baker of Apple Valley															</blockquote>
+						</div>
+						<p align="center"><a class="bottom-link" href="/testimonials/">See More Testimonials &raquo;</a></p>
+									</div>
+				<div class="clear-left"></div>
+			</div>		</div>
+	</div><hr />
